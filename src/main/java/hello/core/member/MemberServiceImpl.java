4dedc96967fcba4dda0c68bd1b8f20bf309080d7
JM_CHOI_ <chlwjdals98@gmail.com>
2021-04-13 20:52:26 +0900

    스프링 싱글톤

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,13 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
@@ -10,7 +15,13 @@ public class MemberServiceImpl implements MemberService {
     public void join(Member member) {
         memberRepository.save(member);
     }
+
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
